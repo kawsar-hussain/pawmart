1d62ee89dd8f4f3d7b51876071ad7b0395532833
kawsar-hussain <mailtokawsarhussain@gmail.com>
2025-12-07 00:34:44 +0600

    updated loader height

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -4,7 +4,7 @@ import HashLoader from "react-spinners/HashLoader";
 const Loader = () => {
   return (
     <div className="flex flex-col">
-      <div className="fixed h-auto lg:h-[90vh] inset-0 flex items-center justify-center bg-white z-50 self-center lg:self-end">
+      <div className="fixed h-auto lg:h-[92vh] inset-0 flex items-center justify-center bg-white z-50 self-center lg:self-end">
         <HashLoader color="#ff9900" size={50} />
       </div>
     </div>
